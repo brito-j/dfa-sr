e9528b99a951368b41025fe22dc9c6ce9227dcd0
Jason <jasonbrito98@gmail.com>
2018-09-20 14:17:35 -0500

    Document code and update styling

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,3 +1,7 @@
+// Jason Brito
+// 9.20.2018
+// The purpose of this component is to handle the category selection.
+
 import {Component, OnInit} from '@angular/core';
 
 @Component({
@@ -13,6 +17,7 @@ export class CategoryComponent implements OnInit {
   ngOnInit() {
   }
 
+  //all possible categories
   categories = [
     'Common Area',
     'Doors/Windows/Blinds/Walls',
@@ -26,21 +31,28 @@ export class CategoryComponent implements OnInit {
     'Plumbing/Water'
   ];
 
+  //currently selected category
   selection = '';
 
+  //search input
   search = '';
 
+  //filters categories based on search input
   filter() {
     return this.categories.filter(c => c.toLowerCase().includes(this.search.toLowerCase()));
   }
 
+  //styles card to indicate current selection
   select(c) {
+    //resets any styling
     for (let i = 0; i < this.filter().length; i++) {
-      document.getElementById(this.filter()[i]).style.boxShadow = "0 .125rem 0 0 #d7d7d7";
-      document.getElementById(this.filter()[i]).style.border = "1px solid #d7d7d7";
+      document.getElementById(this.filter()[i]).style.boxShadow = '0 .125rem 0 0 #d7d7d7';
+      document.getElementById(this.filter()[i]).style.border = '1px solid #d7d7d7';
     }
+    //sets current selection
     this.selection = c;
-    document.getElementById(c).style.boxShadow = "0 .125rem 0 0 #0094d2";
-    document.getElementById(c).style.border = "1px solid #0094d2";
+    //styles current selection card
+    document.getElementById(c).style.boxShadow = '0 .125rem 0 0 #0094d2';
+    document.getElementById(c).style.border = '1px solid #0094d2';
   }
 }
