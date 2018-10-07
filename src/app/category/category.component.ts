4d73d7ff2497a08919af9c6bec2cd8269110823f
Jason <jasonbrito98@gmail.com>
2018-10-07 13:08:51 -0500

    Resolves all conflicts

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -39,17 +39,10 @@ export class CategoryComponent implements OnInit {
     return this.categories.filter(c => c.toLowerCase().includes(this.search.toLowerCase()));
   }
 
-<<<<<<< HEAD
   // Function that bolds matching letters
   bold(c) { return c.replace(new RegExp(this.search, 'gi'), '<b>' + '$&' + '</b>'); }
 
   // Function to obtain location option from click
   onClick(location) { return this.search = location; }
-=======
-  //bolds the letters of the category options from the input search
-  bold(c) { return c.replace(new RegExp(this.search, 'gi'), '<b>' + '$&' + '</b>'); }
 
-  //sets the dropdown button label to the currently selected category
-  setSelection() { return this.selection.length ? this.selection : 'Category'; }
->>>>>>> 83911b2b3d530ad5e5cd51cdae782afd294aeda3
 }
